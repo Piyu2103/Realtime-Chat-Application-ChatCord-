74aed69bc212a10b1fdb7d328e58147455ae575e
Piyush Singhal <singhalpiyush21@gmail.com>
2021-03-24 15:08:16 +0530

    Changed PORT syntax

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ io.on('connection', socket => {
 });
 
 app.use(express.static(staticDirectory));
-const port = 3000 || process.env.PORT;
+const port =  process.env.PORT || 3000 ;
 
 server.listen(port, () => {
     console.log('Server running on ' + port)
